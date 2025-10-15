fe35ad2c57b92e9784b94dd9be8e12c80af286a6
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-15 11:53:41 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return jsonify({"mensaje": "¡Hola! Mi práctica funciona"})
+
+@app.route('/salud')
+def salud():
+    return jsonify({"estado": "ok"})
+
+@app.route('/secreto')
+def secreto():
+    # Esto simula obtener un secreto de Azure Key Vault
+    secreto_simulado = "***CONEXION_BD_OCULTA***"
+    return jsonify({
+        "secreto_enmascarado": secreto_simulado,
+        
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
